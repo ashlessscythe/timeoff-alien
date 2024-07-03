b73b4def82213feeca1da0c34e15a5f3d8979331
Ashless Scythe <drakesav@gmail.com>
2024-07-03 22:17:46 +0000

    added last_name_first to companies in db along with migrations

--- migrations/20240612153328-add-auto-approve-and-manager-only-to-leave-type.js
+++ migrations/20240612153328-add-auto-approve-and-manager-only-to-leave-type.js
@@ -2,16 +2,20 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('leave_types', 'manager_only', {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-      comment:
-        'If true, this leave type can only be used by managers/supervisors'
-    })
+    // Check if the column already exists before adding it
+    const tableInfo = await queryInterface.describeTable('leave_types')
+    if (!tableInfo['manager_only']) {
+      await queryInterface.addColumn('leave_types', 'manager_only', {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+        comment:
+          'If true, this leave type can only be used by managers/supervisors'
+      })
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.query.removeColumn('leave_types', 'manager_only')
+    await queryInterface.removeColumn('leave_types', 'manager_only')
   }
 }
