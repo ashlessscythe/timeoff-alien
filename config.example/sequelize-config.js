bf6c3abb7361744f8d3da668191c15d011fe700c
Ashless Scythe <drakesav@gmail.com>
2024-07-25 17:17:14 +0000

    adding sequelizerc and config to config.exampledir

--- config.example/sequelize-config.js
+++ config.example/sequelize-config.js
@@ -3,12 +3,24 @@ require('dotenv').config()
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
-    dialect: process.env.DB_DIALECT || 'postgres'
+    dialect: process.env.DB_DIALECT || 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: process.env.DB_SSL_REQUIRE === 'true',
+        rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED !== 'false' // Defaults to true unless explicitly set to 'false'
+      }
+    }
     // Additional config settings as needed
   },
   production: {
     url: process.env.DATABASE_URL,
-    dialect: process.env.DB_DIALECT || 'postgres'
+    dialect: process.env.DB_DIALECT || 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Set to true if you have a proper SSL certificate
+      }
+    }
     // Additional production-specific settings
   }
   // Additional environment configurations as needed
