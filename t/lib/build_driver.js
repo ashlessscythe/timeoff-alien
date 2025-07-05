7b8689b9c9f4eb4114cfe27a14659776334da8a3
Ashless Scythe <drakesav@gmail.com>
2025-07-05 20:41:27 +0000

    some testing stuff

--- t/lib/build_driver.js
+++ t/lib/build_driver.js
@@ -1,22 +1,33 @@
 const webdriver = require('selenium-webdriver')
 const chrome = require('selenium-webdriver/chrome')
-const capabilities = process.env.USE_CHROME ? 'chrome' : 'phantomjs'
+const firefox = require('selenium-webdriver/firefox')
 
-module.exports = function() {
-  if (capabilities === 'phantomjs') {
+module.exports = function () {
+  // Default to Chrome, but allow Firefox if specified
+  const browser = process.env.USE_FIREFOX ? 'firefox' : 'chrome'
+
+  if (browser === 'firefox') {
+    const options = new firefox.Options()
+    if (!process.env.SHOW_BROWSER) {
+      options.headless()
+    }
+
     return new webdriver.Builder()
-      .withCapabilities(webdriver.Capabilities[capabilities]())
+      .forBrowser('firefox')
+      .setFirefoxOptions(options)
       .build()
   }
 
   const options = new chrome.Options()
-  if (!process.env.SHOW_CHROME) {
+  if (!process.env.SHOW_BROWSER) {
     options.addArguments('headless')
     options.addArguments('disable-gpu')
+    options.addArguments('no-sandbox')
+    options.addArguments('disable-dev-shm-usage')
   }
 
   return new webdriver.Builder()
-    .withCapabilities(webdriver.Capabilities[capabilities]())
+    .forBrowser('chrome')
     .setChromeOptions(options)
     .build()
 }
--- t/unit/slack.js
+++ t/unit/slack.js
@@ -3,28 +3,28 @@
 const expect = require('chai').expect
 const _ = require('underscore')
 const bluebird = require('bluebird')
-const Slack = require('../../lib/email')
+const Slack = require('../../lib/slack')
 
-describe('Check Slack', function() {
-  it('Knows how to render and parse template', function(done) {
-    var Slack = new Slack()
+describe('Check Slack', function () {
+  it('Knows how to render and parse template', function (done) {
+    var slack = new Slack()
 
     bluebird
       .resolve(
-        Slack.promise_rendered_slack_template({
+        slack.promise_rendered_slack_template({
           template_name: 'foobar',
           context: {
             user: {
               name: 'FOO',
-              reload_with_session_details: function() {
-                bluebird.resolve(1)
+              reload_with_session_details: function () {
+                return bluebird.resolve(1)
               }
             }
           }
         })
       )
-      .then(function(email) {
-        expect(Slack.text).to.match(/Hello FOO\./)
+      .then(function (slack_obj) {
+        expect(slack_obj.text).to.match(/Hello FOO\./)
 
         done()
       })
