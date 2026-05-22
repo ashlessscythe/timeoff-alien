0f087683e1e099396926a67dd2437263394420eb
ashlessscythe <alien@mbook.local>
2026-05-22 13:57:14 -0600

    cookie banner

--- app.js
+++ app.js
@@ -131,6 +131,8 @@ app.use(function(req, res, next) {
   res.locals.url_to_the_site_root = '/'
   // set header from env or default to static string
   res.locals.header_title = process.env.HEADER_TITLE || 'Time Off Requests'
+  res.locals.cookie_banner_enabled =
+    process.env.COOKIE_BANNER_ENABLED !== 'false'
   // set branding url
   res.locals.branding_url = process.env.BRANDING_URL || 'https://timeoff.com'
   // For book leave request modal
